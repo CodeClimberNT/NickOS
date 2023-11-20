d9ce181d00edc924e4065496f8f55c38e43cc9a2
Nicolò Taormina <nicktaormina3@gmail.com>
2023-11-20 11:02:25 +0100

    enable wifi

--- configuration.nix
+++ configuration.nix
@@ -465,7 +465,7 @@ in
       ];
     };
     #   wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
+    wireless.userControlled.enable = true;
     # Configure network proxy if necessary
     # proxy.default = "http://user:password@proxy:port/";
     # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
